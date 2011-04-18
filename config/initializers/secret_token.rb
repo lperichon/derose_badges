82783b658e69befd1baaff1859b849d2ef6211ae
Luis Perichon <luisperichon+github@gmail.com>
2011-04-18 15:53:03 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeroseBadges::Application.config.secret_token = '9e86e2adeab750a57224c34b9234076d63559c752ce0c917b155891d012aba99d38a45ff5a1c5996a88950f11b361c11e6a10d8b3acb007e9220f407effbcbd1'
