f9f442319932be68d1021243cee1d688f249969b
Luis Perichon <luisperichon+github@gmail.com>
2011-05-09 13:39:14 -0300

    forgot schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110506144931) do
+ActiveRecord::Schema.define(:version => 20110509135933) do
 
   create_table "badges", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture_url"
+    t.string   "caption"
+    t.text     "description"
   end
 
   create_table "badges_users", :id => false, :force => true do |t|
